edc480e603aed57b5d7a4bbf20bb54b88572f6c5
Aditya Eka Pranata <aepranata@gmail.com>
2023-11-03 05:36:33 +0700

    rosy: Add support for AfterLife
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,17 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_rosy.mk
+    $(LOCAL_DIR)/afterlife_rosy.mk
 
 COMMON_LUNCH_CHOICES := \
+    afterlife_rosy-user \
+    afterlife_rosy-userdebug \
+    afterlife_rosy-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/banana_rosy.mk
+
+COMMON_LUNCH_CHOICES += \
     banana_rosy-user \
     banana_rosy-userdebug \
     banana_rosy-eng
---
+++ afterlife_rosy.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from rosy device
+$(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
+
+# Inherit some common BananaDroid stuff
+$(call inherit-product, vendor/afterlife/config/common.mk)
+
+AFTERLIFE_MAINTAINER := aepranata
+AFTERLIFE_GAPPS := true
+AFTERLIFE_CORE := true
+AFTERLIFE_EXTRA := true
+BUILD_GALLERYGO := true
+BUILD_GMAIL := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := afterlife_rosy
