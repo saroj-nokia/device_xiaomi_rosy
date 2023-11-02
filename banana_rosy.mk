05dc635b338fc6c0809c5a874c613c8b8ea84a29
Aditya Eka Pranata <aepranata@gmail.com>
2023-11-03 05:32:42 +0700

    rosy: Adapt to new our tree
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- banana_rosy.mk
+++ banana_rosy.mk
@@ -4,10 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+# Inherit from rosy device
+$(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
 
 # Inherit some common BananaDroid stuff
 $(call inherit-product, vendor/banana/config/common.mk)
@@ -15,36 +13,5 @@ $(call inherit-product, vendor/banana/config/common.mk)
 BANANA_MAINTAINER := aepranata
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# Inherit from rosy device
-$(call inherit-product, device/xiaomi/rosy/device.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := banana_rosy
-PRODUCT_DEVICE := rosy
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 5
-PRODUCT_MANUFACTURER := Xiaomi
-
-# Boot animation
-TARGET_BOOTANIMATION_NAME := 720
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true 
-TARGET_BOOTANIMATION_PRELOAD := true 
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
-
-# GApps
-TARGET_GAPPS_ARCH := arm64
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-TARGET_VENDOR_PRODUCT_NAME := rosy
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="rosy-user 7.1.2 N2G47H V9.2.3.0.NDAMIEK release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/rosy/rosy:7.1.2/N2G47H/V9.2.3.0.NDAMIEK:user/release-keys"
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
