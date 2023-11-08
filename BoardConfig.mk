39c5008fbe1c0007e6b7815bb5528b7b921be886
Aditya Eka Pranata <aepranata@gmail.com>
2023-11-09 04:57:29 +0700

    rosy: Switch to atiga defconfig
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_CONFIG := rosy-perf_defconfig
+TARGET_KERNEL_CONFIG := rosy-atiga_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/rosy
 #TARGET_KERNEL_NEW_GCC_COMPILE := true
 #KERNEL_MAKE_FLAGS += CPATH="/usr/include:/usr/include/x86_64-linux-gnu"
