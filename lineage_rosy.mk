6e240b2720584d94e71b183a71a4e82da3a28b37
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2024-07-21 21:11:44 +0545

    update tree

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -14,11 +14,33 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rosy device
-$(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
+$(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_rosy
+PRODUCT_DEVICE := rosy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 5
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Bootanimation
+TARGET_BOOTANIMATION_NAME := 720
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := rosy
+
